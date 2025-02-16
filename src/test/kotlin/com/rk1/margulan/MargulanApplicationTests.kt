81baf9ac3a32b68e68e11b35034c7322019425b2
Margulan <m.begzhan@aster.kz>
2025-02-16 20:22:48 +0500

    init

---
+++ src/test/kotlin/com/rk1/margulan/MargulanApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.rk1.margulan
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MargulanApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
